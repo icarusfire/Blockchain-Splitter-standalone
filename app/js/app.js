d58774a3fe777cb4ddb85603e300c010fb0097f2
icarus fire <icaruss@icaruss-MacBook-Pro-2.local>
2019-11-10 09:41:14 +0100

    add send button

--- app/js/app.js
+++ app/js/app.js
@@ -46,10 +46,70 @@ window.addEventListener('load', function() {
         .then(balance => {
 			console.log("Account Balance", balance);
 			$("#balance2").html(balance.toString(10));
-			
 		})
-		
+        .then(() => $("#send").click(split))
         // Never let an error go unlogged.
         .catch(console.error);
 });
   
+const split = function() {
+    // Sometimes you have to force the gas amount to a value you know is enough because
+    // `web3.eth.estimateGas` may get it wrong.
+    const gas = 300000; let deployed;
+    // We return the whole promise chain so that other parts of the UI can be informed when
+    // it is done.
+    return Splitter.deployed()
+        .then(_deployed => {
+            deployed = _deployed;
+            // We simulate the real call and see whether this is likely to work.
+            // No point in wasting gas if we have a likely failure.
+            return _deployed.splitEther.call( $("input[name='recipient1']").val(), $("input[name='recipient2']").val(),
+
+                // Giving a string is fine
+                //$("input[name='amount']").val(),
+                { from: window.account, gas: gas, value: $("input[name='amount']").val()});
+        })
+        .then(success => {
+            if (!success) {
+                throw new Error("The transaction will fail anyway, not sending");
+            }
+            console.log("starting split..");
+            // Ok, we move onto the proper action.
+            return deployed.splitEther(
+                $("input[name='recipient1']").val(),
+                // Giving a string is fine
+                $("input[name='recipient2']").val(),
+                { from: window.account, gas: gas, value: $("input[name='amount']").val()})
+                // .sendCoin takes time in real life, so we get the txHash immediately while it 
+                // is mined.
+                .on(
+                    "transactionHash",
+                    txHash => $("#status").html("Transaction on the way " + txHash)
+                );
+        })
+        // Now we wait for the tx to be mined.
+        .then(txObj => {
+            const receipt = txObj.receipt;
+            console.log("got receipt", receipt);
+            if (!receipt.status) {
+                console.error("Wrong status");
+                console.error(receipt);
+                $("#status").html("There was an error in the tx execution, status not 1");
+            } else if (receipt.logs.length == 0) {
+                console.error("Empty logs");
+                console.error(receipt);
+                $("#status").html("There was an error in the tx execution, missing expected event");
+            } else {
+                console.log(receipt.logs[0]);
+                $("#status").html("Transfer executed");
+            }
+            // Make sure we update the UI.
+            return web3.eth.getBalance(window.account);
+
+        })
+        .then(balance => $("#balance").html(balance.toString(10)))
+        .catch(e => {
+            $("#status").html(e.toString());
+            console.error(e);
+        });
+};
